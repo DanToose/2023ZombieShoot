d44a776182de570baa08181d9833b6b585dc392b
Dan Toose <dantoose@gmail.com>
2023-05-03 16:21:53 +1000

    Fixed PlayerDamage to allow different scales
    Note - You may have to edit the script if you want more than 3 enemy types, or have them tagged with personal tags.

--- Assets/Scripts/PlayerDamage.cs
+++ Assets/Scripts/PlayerDamage.cs
@@ -11,6 +11,10 @@ public class PlayerDamage : MonoBehaviour
     GameObject LevelManager;
     public GameObject painEffect;
     public float painTimer = 0.0f;
+    public float damageScaleNormal = 1.0f;
+    public float damageScaleElite1 = 1.5f;
+    public float damageScaleElite2 = 3.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,19 +38,32 @@ public class PlayerDamage : MonoBehaviour
     {
         if (collision.gameObject.tag == "Enemy")
         {
-            timer = timer + Time.deltaTime;
-            painTimer = painTimer - Time.deltaTime;
-            if (timer >= 0.05)
-            {
-                health = health - 1 * LevelManager.GetComponent<LevelManager>().damageMultiplyer;
-                timer = 0.0f;
-            }
+            HurtMeDaddy(damageScaleNormal);
+        }
+        else if (collision.gameObject.tag == "EnemyStrong")
+        {
+            HurtMeDaddy(damageScaleElite1);
+        }
+        else if (collision.gameObject.tag == "EnemyBoss")
+        {
+            HurtMeDaddy(damageScaleElite2);
+        }
+    }
 
-            if (painTimer <= 0.0)
-            {
-                Instantiate(painEffect, transform.position, Quaternion.identity);
-                painTimer = 1.0f;
-            }
+    public void HurtMeDaddy(float scale)
+    {
+        timer = timer + Time.deltaTime;
+        painTimer = painTimer - Time.deltaTime;
+        if (timer >= 0.05)
+        {
+            health = health - 1 * LevelManager.GetComponent<LevelManager>().damageMultiplyer * scale;
+            timer = 0.0f;
+        }
+
+        if (painTimer <= 0.0)
+        {
+            Instantiate(painEffect, transform.position, Quaternion.identity);
+            painTimer = 1.0f;
         }
     }
 }
